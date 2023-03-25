24b649af215b9fec947fb469de4711bcef2cea89
Filipwale <97770952+Filipwale@users.noreply.github.com>
2023-03-25 09:17:53 +0100

    Update how-it-works.tsx

--- src/pages/how-it-works.tsx
+++ src/pages/how-it-works.tsx
@@ -7,7 +7,7 @@ const HowItWorks = () => {
   return (
     <PageContainer maxWidth="container.md">
       <Head>
-        <title>AI Avatar: how it works - Photoshot</title>
+        <title>Photostudio | How it works</title>
       </Head>
 
       <VStack
